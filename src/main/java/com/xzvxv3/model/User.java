63ba7cf8802413385a2f2ce32006118ecf14283c
xzvxv3 <xzvxv3@gmail.com>
2025-12-29 21:48:21 +0900

    model - 2

--- src/main/java/com/xzvxv3/model/User.java
+++ src/main/java/com/xzvxv3/model/User.java
@@ -1,15 +1,13 @@
 package com.xzvxv3.model;
 
 public class User {
-    private int userNo;
+    private int userNo; // Primary Key
     private String id;
     private String password;
-    private String nickname;
 
-    public User(String id, String password, String nickname) {
+    public User(String id, String password) {
         this.id = id;
         this.password = password;
-        this.nickname = nickname;
     }
 
     public int getUserNo() { return userNo; }
@@ -20,7 +18,4 @@ public class User {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
-    public String getNickname() { return nickname; }
-    public void setNickname(String nickname) { this.nickname = nickname; }
 }
