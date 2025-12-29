eedc0796e8119e4bd56397ec334ef90078ac9da3
xzvxv3 <xzvxv3@gmail.com>
2025-12-29 16:50:15 +0900

    DB - 1

---
+++ src/main/java/com/xzvxv3/util/DBUtil.java
@@ -0,0 +1,37 @@
+package com.xzvxv3.util;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+// 데이터베이스 관리 클래스
+public class DBUtil {
+
+    // DB 접속 정보를 담을 설정 객체
+    private static Properties props = new Properties();
+
+    static {
+        try (InputStream is = new FileInputStream("src/main/resources/db.properties")) {
+            if (is == null) {
+                System.err.println("db.properties Not Found.");
+                System.exit(0);
+            } else {
+                props.load(is);
+                System.out.println("db.properties Load Success.");
+            }
+        } catch (IOException e) {
+            System.err.println("DB 설정 파일을 읽는 중 오류 발생");
+            e.printStackTrace();
+        }
+    }
+
+    // DB 연결
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                props.getProperty("db.url"),
+                props.getProperty("db.user"),
+                props.getProperty("db.password")
+        );
+    }
+}
